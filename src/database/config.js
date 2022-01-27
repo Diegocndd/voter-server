10fe294ec4e63c55299c240b6513f1391a13df81
Diego Cândido <diegocndd4@gmail.com>
2022-01-27 08:54:15 -0300

    Remoção de comentários

--- src/database/config.js
+++ src/database/config.js
@@ -1,14 +1,6 @@
 require('dotenv').config();
 const mysql = require('mysql');
 
-// let con = mysql.createConnection({
-//     host: process.env.HOST,
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//     timezone: 'utc',
-// });
-
 var con  = mysql.createPool({
     connectionLimit : 2,
     host: process.env.HOST,
@@ -16,22 +8,6 @@ var con  = mysql.createPool({
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
   });
-
-// con.on('error', err => {
-//     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//         console.log('kkk');
-//         disconnect_handler();
-//         con = mysql.createConnection({
-//             host: process.env.HOST,
-//             user: process.env.USER,
-//             password: process.env.PASSWORD,
-//             database: process.env.DATABASE,
-//             timezone: 'utc',
-//         });
-//     } else {
-//         throw err;
-//     }
-// });
 
 module.exports = {
     con,
