ddb57fb87481d18042fc7c52d63bc305f936e076
Diego Cândido <diegocndd4@gmail.com>
2022-02-09 17:20:00 -0300

    teste actions

--- test/password.spec.js
+++ test/password.spec.js
@@ -3,7 +3,7 @@ const { isPasswordValid } = require('../src/utils/dataValidation');
 
 describe('Password Validation', function() {
     it('Password validation function is wrong', function() {
-        const testPassword = '1234';
+        const testPassword = '12';
         const expectedResult = true;
         const validation = isPasswordValid(testPassword);
         expect(validation).toBe(expectedResult);
